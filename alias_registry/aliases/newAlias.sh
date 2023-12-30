8448f6c68adc3a3ebbf602b87a38279eb05d9c15
deanofwalls <deanofwalls@gmail.com>
2023-12-30 11:10:46 -0500

    Added backup of network bridge configuration and README.md

--- alias_registry/aliases/newAlias.sh
+++ alias_registry/aliases/newAlias.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # Check if one argument is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: newAlias <aliasCommandName>"
+    echo -e "\e[38;5;205mUsage: newAlias <aliasCommandName>\e[0m"
     exit 1
 fi
 
@@ -11,21 +13,23 @@ ALIAS_REGISTRY=~/alias_registry.sh
 
 # Check if the alias already exists in alias_registry.sh
 if grep -q "^alias $ALIAS_NAME=" "$ALIAS_REGISTRY"; then
-    echo "Alias '$ALIAS_NAME' already exists."
+    echo -e "\e[38;5;205mAlias '$ALIAS_NAME' already exists.\e[0m"
     exit 1
 fi
 
 # Check if the script file already exists
 if [ -f "$ALIAS_DIR/$SCRIPT_NAME" ]; then
-    echo "Shell script '$SCRIPT_NAME' already exists in $ALIAS_DIR."
+    echo -e "\e[38;5;205mShell script '$SCRIPT_NAME' already exists in $ALIAS_DIR.\e[0m"
     exit 1
 fi
 
 # Create the new script file
 echo "#!/bin/bash" > "$ALIAS_DIR/$SCRIPT_NAME"
-chmod +x "$ALIAS_DIR/$SCRIPT_NAME"
 
 # Add the alias to alias_registry.sh
 echo "alias $ALIAS_NAME='$ALIAS_DIR/$SCRIPT_NAME \$@'" >> "$ALIAS_REGISTRY"
 
-echo -e "\e[38;5;205mAlias $ALIAS_NAME created succesfully!\e[0m"
+# Update permissions for all files in ALIASES_DIR
+find "$ALIAS_DIR" -type f -exec chmod +x {} \;
+
+echo -e "\e[38;5;205mAlias $ALIAS_NAME created successfully!\e[0m"
