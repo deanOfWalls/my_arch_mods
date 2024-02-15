8311311ee76b9d89eb40e5dea8027fd1ee818d35
deanofwalls <deanofwalls@gmail.com>
2024-02-14 23:01:00 -0500

    Updated battery alias

--- alias_registry/aliases/battery.sh
+++ alias_registry/aliases/battery.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-upower -i /org/freedesktop/UPower/devices/headphones_dev_23_05_10_C4_13_EC
+upower -i /org/freedesktop/UPower/devices/headphones_dev_23_05_10_C4_13_EC | grep -E "model|percentage" | awk '{print "\033[1;36m" $1 ":\033[0m", "\033[1;33m" $2 "\033[0m"}'
--- alias_registry/aliases/listen.sh
+++ alias_registry/aliases/listen.sh
@@ -2,14 +2,14 @@
 
 # Define colors
 color_start_red="\033[38;5;196m"
-color_start_pink="\033[38;5;205m"
 color_start_blue="\033[38;5;33m"
+color_start_pink="\033[38;5;205m"
 color_end="\033[0m"
 
 # Function to search YouTube and present top 12 results
 search_and_present_results() {
     query="$*"
-    echo -e "${color_start_red}Searching YouTube for: $query...${color_end}"
+    echo -e "${color_start_blue}Searching YouTube for: $query...${color_end}"
     search_results=$(yt-dlp "ytsearch12:$query" --get-title --get-id --skip-download --quiet 2>/dev/null)
 
     # Check if search results are empty or contain error message
@@ -42,11 +42,6 @@ play_song() {
     MPV_PID=$!
 }
 
-# Function to stop playing the song
-stop_song() {
-    kill $MPV_PID
-}
-
 # Main script
 if [ -z "$1" ]; then
     echo "Usage: $0 <search_query>"
@@ -55,23 +50,12 @@ fi
 
 search_and_present_results "$@"
 
-echo "To stop the song, run 'stop' command."
-echo "To pause the song, run 'pause' command."
+echo "To exit, use Ctrl+C."
 
 while :
 do
     read -p "> " selection
     case "$selection" in
-        stop)
-            stop_song
-            exit
-            ;;
-        pause)
-            kill -STOP $MPV_PID
-            ;;
-        resume)
-            kill -CONT $MPV_PID
-            ;;
         [1-9]|10|11|12)
             play_song "$selection"
             ;;
